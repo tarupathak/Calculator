c64fb06af98c70c9fa331e4207e290df5c228b02
tarupathak <113367480+tarupathak@users.noreply.github.com>
2022-10-29 01:33:12 +0530

    Added calculator files

---
+++ script.js
@@ -0,0 +1,32 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string)
+            document.querySelector('input').value = string;
+            // if(e.target.innerHTML == 'M+')
+            // {
+            //     string = eval(string) + string;
+            //     document.querySelector('input').value = string;
+            // }
+            // else{
+            //     string = string - eval(string);
+            //     document.querySelector('input').value = string;
+            // }
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = ""
+            document.querySelector('input').value = string;
+        }
+       
+        else {
+            console.log(e.target);
+           
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+
+})
